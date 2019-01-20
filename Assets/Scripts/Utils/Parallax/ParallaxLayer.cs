0788448bdac4bc06c291497202b79968e2a15a8c
zHaytam <haytam.zanid@gmail.com>
2019-01-20 21:17:44 +0100

    Added placeholder "level" + camera follow + parallax effect + game boundaries

---
+++ Assets/Scripts/Utils/Parallax/ParallaxLayer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Utils.Parallax
+{
+    [ExecuteInEditMode]
+    public class ParallaxLayer : MonoBehaviour
+    {
+
+        public float parallaxFactor;
+
+        public void Move(float delta)
+        {
+            Vector3 newPos = transform.localPosition;
+            newPos.x -= delta * parallaxFactor;
+            transform.localPosition = newPos;
+        }
+
+    }
+}
